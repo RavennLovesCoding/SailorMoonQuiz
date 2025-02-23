c707e6d006b0d794d9d1ee7c92fb45441dffcb36
Ravenn <ravennmb@gmail.com>
2025-02-23 12:39:23 -0700

    correct answer will be highlighted when wrong choice selected.

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import './Quiz.css'
 import { data } from '../../assets/data';
 
@@ -8,6 +8,13 @@ const Quiz = () => {
     let [question, setQuestion] = useState(data[index]);
     let [choice, setChoice] = useState(false);
 
+    let Option1 = useRef(null);
+    let Option2 = useRef(null);
+    let Option3 = useRef(null);
+    let Option4 = useRef(null);
+
+    let option_array = [Option1, Option2, Option3, Option4];
+
     const checkAns = (e, ans)=> {
         if(choice === false){
             if(question.ans===ans) {
@@ -16,9 +23,14 @@ const Quiz = () => {
             } else{
                 e.target.classList.add("wrong");
                 setChoice(true);
+                option_array[question.ans-1].current.classList.add("correct"); //highlight the correct answer if the user clicks the wrong answer
             }
         }
     }
+
+    const next = () =>{
+        
+    } 
 
   return (
     <>
@@ -27,10 +39,10 @@ const Quiz = () => {
         <hr />
         <h2>{index+1}. {question.question}</h2>
         <ul>
-            <li onClick={(e)=>{checkAns(e,1)}}>{question.option1}</li>
-            <li onClick={(e)=>{checkAns(e,2)}}>{question.option2}</li>
-            <li onClick={(e)=>{checkAns(e,3)}}>{question.option3}</li>
-            <li onClick={(e)=>{checkAns(e,4)}}>{question.option4}</li>
+            <li ref={Option1} onClick={(e)=>{checkAns(e,1)}}>{question.option1}</li>
+            <li ref={Option2} onClick={(e)=>{checkAns(e,2)}}>{question.option2}</li>
+            <li ref={Option3} onClick={(e)=>{checkAns(e,3)}}>{question.option3}</li>
+            <li ref={Option4} onClick={(e)=>{checkAns(e,4)}}>{question.option4}</li>
         </ul>
         <button>Next</button>
         <div className="index">{index+1} of 5 questions</div>
